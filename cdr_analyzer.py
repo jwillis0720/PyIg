0e4fd7a56462fc822261112b4b2bf12fd0a33d36
Jordan Willis <jwillis0720@gmail.com>
2013-10-30 16:13:49 -0500

    Adding split_fasta that I left out

---
+++ cdr_analyzer.py
@@ -0,0 +1,7 @@
+#
+class cdr_analyzer():
+	def __init(self,initial_json_dictionary):
+		self.unmodified = self.initial_json_dictionary
+	
+	def return_json_dict_with_cdr3_analysis(self,initial_json_dictionary):
+		print self.unmodified
---
+++ split_fasta.py
@@ -0,0 +1,42 @@
+def split_fasta(num_procs,file_name,suffix="tmp.fasta"):
+        '''Split the file name by the number of processors you specify
+
+        arguments (num_procs - The amount of processors you are running, lets the function know how much to split up the file into with one file per processor)'''
+
+        file_prefix = file_name.split('.fasta')[0]
+        print "Counting entries in fasta files..."
+        parent_file = list(Bio.SeqIO.parse(file_name, 'fasta'))
+        length_parent_file = len(parent_file)
+        print "{0} in fasta file".format(length_parent_file)
+
+        files_per_tmp = float(length_parent_file) / float(num_procs)
+        print "{0} processors, blasting {1} entries per processor".format(num_procs, files_per_tmp)
+
+        # carries all of our records to be written
+        joiner = []
+        file_counter = 1
+        num = 1
+
+        # enumerate huge fasta
+        for record in parent_file:
+            # append records to our list holder
+            joiner.append(">" + record.id + "\n" + str(record.seq))
+            # if we have reached the maximum numbers to be in that file, write
+            # to a file, and then clear
+            if num > files_per_tmp:
+                joiner.append("")
+                with open(file_prefix + str(file_counter) + suffix, 'w') as f:
+                    f.write("\n".join(joiner))
+                # change file name,clear record holder, and change the file
+                # count
+                joiner = []
+                file_counter += 1
+                num = 1
+            else:
+                num += 1
+        if joiner:
+            # for left over fasta entries, very important or else they will
+            # just hang out in limbo
+            joiner.append("")
+            with open(file_prefix + str(file_counter) + suffix, 'w') as f:
+                f.write("\n".join(joiner))
