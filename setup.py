282a943c31cd79444271157d7923969dd72ceb90
jwillis0720VVC1 <jordanwillis@hal9000.dhcp.mc.vanderbilt.edu>
2014-06-05 22:44:07 -0500

    still can't get the database not to choke, might have to write in a manual method

--- setup.py
+++ setup.py
@@ -1,21 +1,33 @@
 #!/usr/bin env python
-from distutils.core import setup
+# from distutils.core import setup
 
-import fnmatch
-import os
+# import fnmatch
+# import os
 
-matches = []
-for root, dirnames, filenames in os.walk(''):
-    for filename in fnmatch.filter(filenames, '*'):
-        matches.append(os.path.join(root, filename))
+# matches = []
+# for root, dirnames, filenames in os.walk(''):
+#     for filename in fnmatch.filter(filenames, '*'):
+#         matches.append(os.path.join(root, filename))
 
 
-setup(name='PyIg',
-      version='1.1',
-      description='Python Immunoglobulin Analysis Tools',
-      author='Jordan Willis',
-      author_email='jwillis0720@gmail.com',
-      packages=['pyig.backend', 'pyig.gui', 'pyig.commandline', 'pyig'],
-      package_dir={'': 'src'},
-      #package_data={'': ['database/*']},
-      scripts=['scripts/PyIg_GUI', 'scripts/PyIg'])
+# setup(name='PyIg',
+#       version='1.1',
+#       description='Python Immunoglobulin Analysis Tools',
+#       author='Jordan Willis',
+#       author_email='jwillis0720@gmail.com',
+#       packages=['pyig.backend', 'pyig.gui', 'pyig.commandline', 'pyig'],
+#       package_dir={'': 'src'},
+#       #package_data={'': ['database/*']},
+#       scripts=['scripts/PyIg_GUI', 'scripts/PyIg'])
+
+from setuptools import setup, find_packages
+
+setup(
+    name='PyIg',
+    version='1.1',
+    description='Python Immunoglobulin Analysis Tools',
+    packages=['src/pyig'],
+    package_data={'database': ['*']},
+    include_package_data=True,
+    install_requires=[],
+)
