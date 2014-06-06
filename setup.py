8c392c03fbde444356234258e528cb702800586a
jwillis0720VVC1 <jordanwillis@hal9000.dhcp.mc.vanderbilt.edu>
2014-06-05 22:10:53 -0500

    chokes on the database, but hopefully I will find out how distutils installs it

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 #!/usr/bin env python
 from distutils.core import setup
 
+import fnmatch
+import os
+
+matches = []
+for root, dirnames, filenames in os.walk(''):
+    for filename in fnmatch.filter(filenames, '*'):
+        matches.append(os.path.join(root, filename))
+
+
 setup(name='PyIg',
       version='1.1',
       description='Python Immunoglobulin Analysis Tools',
@@ -8,4 +17,5 @@ setup(name='PyIg',
       author_email='jwillis0720@gmail.com',
       packages=['pyig.backend', 'pyig.gui', 'pyig.commandline', 'pyig'],
       package_dir={'': 'src'},
+      #package_data={'': ['database/*']},
       scripts=['scripts/PyIg_GUI', 'scripts/PyIg'])
