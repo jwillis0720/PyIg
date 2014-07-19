7c6cc4327cf835e674109aaa5698563d73bb7916
Jordan Willis <jordanwillis@dhcp-229-55.scripps.edu>
2014-07-18 18:49:57 -0700

    Adding functionality to the setup script that automatically installs igblastn on the backend (/usr/local/bin/), works for mac and linux centos and linux redhat. Will get other flavors of linux eventually. Check NCBI website for the precompiled build. Eventually this will build from system to system so we don't need precompiled binaries..but eh, such is life

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import os
 import pickle
 from shutil import copytree
 import sys
+import platform
 
 library_dir = "/usr/local/lib/"
 pyig_lib = os.path.join(library_dir, 'pyig')
@@ -19,8 +20,26 @@ if not os.path.exists(pyig_lib):
               "to install the library".format(pyig_lib)
         sys.exit(1)
 
-
+#my hack way of knowking the directory location. This is supernice for dynamic code. THERE HAS TO BE A BETTER WAY OF DOING THIS
 pickle.dump(pyig_lib,open('src/library_dir.txt','w'))
+
+
+def get_os():
+  _os, _op = sys.platform, platform.system()
+  if _os == 'darwin':
+    return 'darwin'
+  
+  elif _os == 'Linux2':
+    if _op[0] == 'centos':
+      return 'centos'
+    elif _op[0] == 'redhat':
+      return 'redhat'
+    else:
+      raise Exception("Can't find your operating system...I only have redhat, mac, and centos in my files for now. Contact the higherups.")
+  else:
+    raise Exception("Can't find your architecture...I only have mac, and linux supported in my files for now. Contact the higherups.")
+
+
 setup(name='PyIg',
       version='1.1',
       description='Python Immunoglobulin Analysis Tools',
@@ -29,4 +48,4 @@ setup(name='PyIg',
       packages=['pyig.backend', 'pyig.gui', 'pyig.commandline', 'pyig'],
       package_dir={'': 'src'},
       package_data={'pyig.gui':['library_dir.txt']},
-      scripts=['scripts/PyIg_GUI', 'scripts/PyIg'])
+      scripts=['scripts/PyIg_GUI', 'scripts/PyIg','igblast_source/'+get_os()+"/bin/igblastn"])
