c0619dae39c0452f94e2a80fb9489bbe66f6db39
Jordan Willis <jordanwillis@dhcp-231-56.scripps.edu>
2014-08-04 18:51:27 -0700

    Changing name of argument parser class

--- setup.py
+++ setup.py
@@ -1,45 +1,47 @@
 #!/usr/bin env python
 from distutils.core import setup
 import os
-import pickle
-from shutil import copytree, rmtree
+from shutil import copytree, rmtree, copyfile
 import sys
 import platform
-# library_dir = "/usr/local/lib/"
-# pyig_lib = os.path.join(library_dir, 'pyig')
 
-# if os.path.exists(pyig_lib):
-#   print "Deleting old copy of".format(pyig_lib)
-#   rmtree(pyig_lib)
-# try:
-#   print "Trying...copying data to {0}".format(pyig_lib)
-#   copytree('data_files', pyig_lib)
-#   os.putenv('PYIGLIB',library_dir)
-# except OSError:
-#     print "Need root to install in {0} directory location," \
-#           "use --library-path flag to specify where you want" \
-#           "to install the library".format(pyig_lib)
-#     sys.exit(1)
+library_dir = "/usr/local/pyig/data_dir"
+bin = "/usr/local/bin/"
 
-# #my hack way of knowking the directory location. This is supernice for dynamic code. THERE HAS TO BE A BETTER WAY OF DOING THIS
-# pickle.dump(pyig_lib,open('src/pyig/gui/library_dir.txt','w'))
+if os.path.exists(library_dir):
+      print "Deleting old copy of".format(library_dir)
+      rmtree(library_dir)
+try:
+      print "Trying...copying data to {0}".format(library_dir)
+      copytree('data_dir', library_dir)
+except OSError:
+      print "Need root to install in {0} directory location," \
+             "to install the library".format(library_dir)
+      sys.exit(1)
 
 
-# def get_os():
-#   _os, _op = sys.platform, platform.system()
-#   if _os == 'darwin':
-#     return 'darwin'
-  
-#   elif _os == 'Linux2':
-#     if _op[0] == 'centos':
-#       return 'centos'
-#     elif _op[0] == 'redhat':
-#       return 'redhat'
-#     else:
-#       raise Exception("Can't find your operating system...I only have redhat, mac, and centos in my files for now. Contact the higherups.")
-#   else:
-#     raise Exception("Can't find your architecture...I only have mac, and linux supported in my files for now. Contact the higherups.")
+def get_os():
+      _os, _op = sys.platform, platform.system()
+      if _os == 'darwin':
+            return 'darwin'
+      elif _os == 'Linux2':
+            if _op[0] == 'centos':
+                  return 'centos'
+            elif _op[0] == 'redhat':
+                  return 'redhat'
+            else:
+                  raise Exception("Can't find your operating system...Compile yourself")
+      else:
+            raise Exception("Can't find your architecture...I only have mac, and linux supported in my files for now. Contact the higherups.")
 
+#copy igblastn
+src = "igblast/"+get_os()+"/bin/igblastn"
+src = os.path.abspath(src)
+try:
+      copyfile(src,os.path.abspath(bin+"igblastn"))
+      os.chmod(os.path.abspath(bin+"igblastn"), 0755)
+except IOError:
+      raise IOError("You don't have correct permissions, please run as admin")
 
 setup(name='PyIg',
       version='1.1',
@@ -48,18 +50,26 @@ setup(name='PyIg',
       author_email='jwillis0720@gmail.com',
       packages=['pyig.backend', 'pyig.commandline', 'pyig'],
       package_dir={'pyig': 'src/pyig'},
-      package_data = {'pyig':['data_files/*.txt',
-                              'data_files/*.py',
-                              'data_files/database/Ig/human/*',
-                              'data_files/database/Ig/mouse/*',
-                              'data_files/database/TCR/human/*',
-                              'data_files/database/TCR/mouse/*',
-                              'data_files/internal_data/human/*',
-                              'data_files/internal_data/mouse/*',
-                              'data_files/internal_data/rabbit/*',
-                              'data_files/internal_data/rat/*',
-                              'data_files/optional_file/*',
-                              ]},
-      #package_data={'pyig.gui':['library_dir.txt']},
       scripts=['scripts/PyIg'])
+      
+
+
+
+
       #'igblast_source/'+get_os()+"/bin/igblastn"])
+
+
+
+      # #package_data = {'pyig':['data_files/*.txt',
+      #  #                       'data_files/*.py',
+      #                         'data_files/database/Ig/human/*',
+      #                         'data_files/database/Ig/mouse/*',
+      #                         'data_files/database/TCR/human/*',
+      #                         'data_files/database/TCR/mouse/*',
+      #                         'data_files/internal_data/human/*',
+      #                         'data_files/internal_data/mouse/*',
+      #                         'data_files/internal_data/rabbit/*',
+      #                         'data_files/internal_data/rat/*',
+      #                         'data_files/optional_file/*',
+                            #  ]},
+      #package_data={'pyig.gui':['library_dir.txt']},
