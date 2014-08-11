0a21ad42742481d01472e3b3cee3054e31fc905e
Jordan Willis <jordanwillis@dhcp-231-56.scripps.edu>
2014-08-11 16:09:21 -0700

    Added new setup.py, fixed error with import

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin env python
 from shutil import copytree, rmtree, copyfile
 from distutils.core import setup
-import platform
 import os
 import sys
 import glob
 import subprocess
 
-library_dir = "/usr/local/pyig/data_dir"
-bin = "/usr/local/bin/"
+library_dir = os.path.abspath("/usr/local/pyig/data_dir")
+bin_path = os.path.abspath("/usr/local/bin/")
+
+print "Checking Permissions for {0} and {1}".format(library_dir, bin_path)
+if not os.access(library_dir, os.W_OK) and not os.access(bin_path, os.W_OK):
+    raise OSError("Can't install libraries or binaries at {0} and {1}, \
+      permission error, seek administrator".format(
+        library_dir, bin_path))
 
 
 if sys.version_info < (2, 7):
@@ -16,50 +21,44 @@ if sys.version_info < (2, 7):
 
 try:
     import Bio
-except ImportError:
+    print "Have {0}".format(Bio.__file__)
+except Exception:
     raise ImportError("You need the Biopython Package...see documentation")
 
 
 if os.path.exists(library_dir):
     print "Deleting old copy of".format(library_dir)
-    if os.access(library_dir, os.W_OK):
-        rmtree(library_dir)
-        copytree('data_dir', library_dir)
-    else:
-        print "Need to root to install {0}".format(library_dir)
+    rmtree(library_dir)
+    copytree('data_dir', library_dir)
 else:
-    try:
-        copytree('data_dir', library_dir)
-    except OSError:
-        print "Need root to install in {0} directory location to install the library".format(library_dir)
-        sys.exit(1)
+    copytree('data_dir', library_dir)
+
 
 def get_igblast():
     print "Determining OS"
     igblasts = glob.glob('igblast/igblastn_*')
     for binary in igblasts:
-      try:
-        if subprocess.check_call([binary,'-h'],stdout=subprocess.PIPE) == 0:
-          return os.path.abspath(binary)
-      except OSError:
-          continue
-      return ""
+        try:
+            if subprocess.check_call([binary, '-h'],
+                                     stdout=subprocess.PIPE) == 0:
+                return os.path.abspath(binary)
+        except OSError:
+            continue
+        return ""
 
-#copy igblastn
+# copy igblastn
 igblast = get_igblast()
 if igblast:
-  try:
-    new_igblast = os.path.abspath(bin + "igblastn")
-    print "Copying {0} to {1}".format(igblast,new_igblast)
+    new_igblast = os.path.abspath(bin_path + "igblastn")
+    print "Copying {0} to {1}".format(igblast, new_igblast)
     copyfile(igblast, new_igblast)
-    print "Changing directory permissions of {0}".format(new_igblast)
+    print "Changing directory permissions ofss {0}".format(new_igblast)
     os.chmod(new_igblast, 0755)
-  except IOError:
-    raise IOError("You don't have correct permissions, please run as admin")
-
 else:
-  print "We don't have a IgBlast that will run, please see documentation to compile yourself, press anykey to continue"
-  raw_input()
+    print "Don't have a Igblastn that will run, \
+           please see documentation to compile yourself, \
+           press anykey to continue"
+    raw_input()
 
 setup(name='PyIg',
       version='1.1',
@@ -69,4 +68,3 @@ setup(name='PyIg',
       packages=['pyig.backend', 'pyig.commandline', 'pyig'],
       package_dir={'pyig': 'src/pyig'},
       scripts=['src/pyig/commandline/PyIg'])
-
